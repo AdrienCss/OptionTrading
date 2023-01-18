04624cead22f215f5468c2618a0f96f520271a7b
Adrien Cassagnes <49991971+AdrienCss@users.noreply.github.com>
2023-01-18 17:34:56 +0100

    Create mainOptionStrategies.py
    Adding

---
+++ mainOptionStrategies.py
@@ -0,0 +1,32 @@
+from Volatility import ImpliedVolatiliy
+from Option.OptionStrategies import OptionStrategies
+from Option.Option import Option , Stock
+from Enum.OptionType import OpionType
+from Enum.BuySellSide import BuySellSide
+from DataRequest import  y_finane_option_data , y_finane_stock_data
+from Volatility.ImpliedVolatiliy import compute_theorical_IV, plot_ImpliedVolatility
+
+
+
+#Underlying pricen quote
+currentprice = last_price
+r=0.01
+vol = 0.05
+T =1
+
+callOption = Option(price= 3 , K=15 , type= OpionType.CALL)
+callOption2 = Option(price= 1 , K=25 , type= OpionType.CALL)
+#stock = Stock(price = currentprice)
+
+strategy = OptionStrategies(name = "Bear Spread" ,St = currentprice)
+
+strategy.add_Option(option= callOption ,buySell= BuySellSide.BUY , option_number=1 )
+strategy.add_Option(option= callOption2 ,buySell= BuySellSide.SELL , option_number=1 )
+
+strategy.compute_greek_profile(T ,r , vol)
+
+strategy.plot()
+strategy.plotGreek(greekStr='gamma')
+strategy.plotGreek(greekStr='theta')
+strategy.plotGreek(greekStr='delta')
+strategy.plotGreek(greekStr='vega')
